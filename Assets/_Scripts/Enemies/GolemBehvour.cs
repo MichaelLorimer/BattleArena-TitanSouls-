c57769ea19a91e7fc4595b71fec45cf75f95a5cd
MichaelLorimer <michael.lorimer@hotmail.co.uk>
2018-03-25 22:59:44 +0100

    25/03/2018
    Attacks now work

---
+++ Assets/_Scripts/Enemies/GolemBehvour.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemBehvour : MonoBehaviour 
+{
+	// ===========================
+	// ---- To - Do ----
+	//------------------
+
+	//Attacks 
+	// --- short
+	// --- long
+	//Movements 
+	// -- Figure 8TakenDamage
+	// -- Smash
+
+	//States
+
+	// ---- Boss States ----
+	bool m_Idle; 			// Asleep?
+	bool m_Moving;
+	bool m_Attacking; 		// State to pick an attack and initite it
+	bool m_Vulnerable;		// Vecoms vunnerable after attack
+	bool m_Invlunerable; 	//Any time before an attack 
+	bool m_Dead;			//to trigger death animations 
+
+	// Transition- idle-> Invlunerable/ Attacking -> Vulnerable -> back to attacking 
+
+	// ---- Boss Vars ----
+	public int m_Health;	    // Health is setto 1 by default for testing 
+	public Transform m_target;	    //Target for following/ Smashing(player)
+	public float m_speed = 2f; 	    //Movment speed
+
+	public Vector2 m_TargetPos; //Target for following/ Smashing(player)
+
+	// ---- Boss Components ----
+	Rigidbody2D m_BossBody;			//Rigid Body decloration to store the sprite renderer
+	SpriteRenderer m_spriteRender;	//Sprite renderer decloration tostore the sprite renderer
+
+	public Sprite m_DefaultSprite;   	//Default sprite for animating attacks --!!Shouldstic kin the animator later !!--
+	public Sprite m_SmashSprite;		//Smashing hand sprite for animating attacks --!!Shouldstic kin the animator later !!--
+	public Sprite m_VulnerableSprite;   //Vulnerable hand sprite for animating attacks --!!Shouldstic kin the animator later !!--
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Moving = true; 								  //first state currently 
+		m_BossBody = GetComponent<Rigidbody2D> (); 		  //Cache the Rigidbody component
+		m_spriteRender = GetComponent<SpriteRenderer> (); //Cache the Rigidbody component
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Check if the handiswithing a given distance betweenthe player and if not close move towards the player
+		if (Vector2.Distance (m_BossBody.transform.position, m_target.position) > 0.5f) 
+		{
+			Vector3 CurrentPos = m_BossBody.transform.position; 														   //Set Current Posto the RB position of the hand 
+			CurrentPos = Vector3.MoveTowards (m_BossBody.transform.position, m_target.position, m_speed * Time.deltaTime); //Move towards the player
+			m_BossBody.transform.position = CurrentPos; 																   //Apply the new calculatedpositon to the golemhand
+		}
+
+		//Set the Sprite to a smash sprite and activates the players apply damagefuntion fora knockback effect
+		if (Vector2.Distance (m_BossBody.transform.position, m_target.position) < 1f) 
+		{
+			m_spriteRender.sprite = m_SmashSprite; //Set theCurrent Sprite to the Smash sprite to apply damage --!! Should do thiswith animator later !!--
+			PlayerScript.TakenDamage ();		   //Actvates the players take damage function for a knock back effect --!! Should do this in player sript later !!--
+		}
+			
+		if (m_Health <= 0) 
+		{
+			m_speed = 0f;
+			//change sprite later
+		}
+	}
+
+	// --!! Will be used to pick which action totake !!--
+	void PickAction()
+	{
+
+	}
+
+	// Attack method 
+	void Attacks()
+	{
+		//Smash
+
+
+		//---- Follow ---
+		/// -- Follow player
+		/// --move down whenabove player y within 10 units?
+		/// move down to players x 
+		/// deal damage
+		/// become vulnerable
+
+		//BossBody.position
+		Vector2 currentPos = m_BossBody.position;       //The current golem position
+		Vector2 TargetPos = PlayerScript.GetPosition(); //Get the player position
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.name == "Sword") 
+		{
+			m_spriteRender.sprite = m_VulnerableSprite; // --!! change later !!--
+			m_Health--; // Reduce the enemies health by 1 per hit.
+		}
+	}
+}
---
+++ Assets/_Scripts/PlayerChar/PlayerScript.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------- To-Do ---------------
+//-------------------------------------
+
+// -- Main Task --
+// ----- Fix Collision, Slowdown movement
+
+// - Animations
+// --- Attack
+// --- Moving 
+// --- dodging? 
+// --- dieing
+
+// - Player Actions
+// --- Attack
+// --- Dodge?
+// --- Die
+// --- Move -Part Implimented
+
+public class PlayerScript : MonoBehaviour 
+{
+	//------ Player Components ------
+	static Rigidbody2D m_PlayerRB;	//Holds the players RigidBody2D
+	SpriteRenderer m_PlayerSR;  	//Holds the Players SpriteRenderer
+
+	//------ Player Vars ------
+	int m_PlayerHealth; //Holds the player Health (Set in Inspector/ Awake())
+	int m_PlayerLives;  //Holds the player Lives (likely set to 3 by Default in Inspector/Awake())
+	int m_PlayerDamage; //Holds the Attack Value for the player (likely set in inspector/ Awake())
+
+	static public Vector3 pos; // Current position If the player (public so it canbe accessed by enemies)
+
+	//------ Player Bools ------
+	// ----- Actions
+	bool m_Attack = false;   	//Determins if the player is attacking 
+	bool m_Moving = false; 	    //Determins if the player is moving 
+	static bool m_DamageTaken = false; //Determins if the player has been damaged
+
+	// ----- Game States
+	bool m_GameOver = false;	//Should possibly be in GameManager Script
+
+	// ---- PlayerVars ----
+	public float speed;			// Player movement speed (Set in the inpector)
+	public int Health = 3;				// Playersstarting health (Should probably be an inpector element later)
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_PlayerRB = GetComponent<Rigidbody2D> (); // Cache and set m_PlayerRB to the ObjectsRidigbody2D
+	}
+
+	//Updatefunction for physics
+	void FixedUpdate()
+	{
+		Moving(); // Calls the movment code
+
+		//Apply a knock back when damage taken --!! Later on: Make it more robust and move in the oposit direction of the damage !!--
+		if (m_DamageTaken == true) 
+		{
+			Vector3 CurrentPos = m_PlayerRB.transform.position; //Assign CurrentPos to the players RB Position
+			CurrentPos.y += -(speed*10) * Time.deltaTime;       //Apply the knock back
+			CurrentPos.z = -1;									//Set the Z level --!! Find out why it resets to 0 Later !!--
+			m_PlayerRB.transform.position = CurrentPos;			//Set the nw modyfied CurrentPos to the RB position
+
+			if (Health > 0) 
+			{
+				Health--; //Reduce players health by 1 per hit	
+			} 
+			else 
+			{
+				m_GameOver = true; //Game over = true if health is 0 or less
+			}
+			m_DamageTaken = false;								//Set damage to falsereadyfor the next hit 
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		pos = m_PlayerRB.transform.position; //Set the Iitial RB Position for enemy follows --!! Change it to a target Transform later !!--
+	}
+
+	// Movemet function for player movement
+	void Moving()
+	{
+		//character will move a set amount without using physics
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			Vector3 CurrentPos = m_PlayerRB.transform.position; //Assign CurrentPos to the players RB Position
+			CurrentPos.y += speed * Time.deltaTime;				//Apply Movement
+			CurrentPos.z = -1;									//Set the Z level --!! Find out why it resets to 0 Later !!--
+			m_PlayerRB.transform.position = CurrentPos;			//Set the nw modyfied CurrentPos to the RB position
+
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			Vector3 CurrentPos = m_PlayerRB.transform.position; //Assign CurrentPos to the players RB Position
+			CurrentPos.x -= speed * Time.deltaTime;				//Apply Movement
+			CurrentPos.z = -1;									//Set the Z level --!! Find out why it resets to 0 Later !!--
+			m_PlayerRB.transform.position = CurrentPos;			//Set the nw modyfied CurrentPos to the RB position
+
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			Vector3 CurrentPos = m_PlayerRB.transform.position; //Assign CurrentPos to the players RB Position
+			CurrentPos.y -= speed * Time.deltaTime;				//Apply Movement
+			CurrentPos.z = -1;									//Set the Z level --!! Find out why it resets to 0 Later !!--
+			m_PlayerRB.transform.position = CurrentPos;			//Set the nw modyfied CurrentPos to the RB position
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			Vector3 CurrentPos = m_PlayerRB.transform.position; //Assign CurrentPos to the players RB Position
+			CurrentPos.x += speed * Time.deltaTime;				//Apply Movement
+			CurrentPos.z = -1;									//Set the Z level --!! Find out why it resets to 0 Later !!--
+			m_PlayerRB.transform.position = CurrentPos;			//Set the nw modyfied CurrentPos to the RB position
+		}
+
+		//Attack button
+		if (Input.GetKey (KeyCode.L)) 
+		{
+			Rigidbody2D sword = SwordDamage.SwordBod;
+			Vector3 SwordPlace = sword.position;
+			SwordPlace.z = -1;
+			SwordPlace = m_PlayerRB.position;
+			SwordPlace.y += 5f;
+			sword.transform.position = SwordPlace;
+		}
+	}
+
+	public static void TakenDamage()
+	{
+		m_DamageTaken = true; //Set to true to activate damage code --!! Wont need when damage is calculated in this script !!--
+	}
+
+	//Getter for positon for things that follow the player 
+	public static Vector3 GetPosition ()
+	{
+		return pos;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Enemy") 
+		{
+			Health--;
+		}
+	}
+}
