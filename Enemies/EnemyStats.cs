f1659de43ff7aa24f9f5d277688ff05f7bff55cf
MichaelLorimer <michael.lorimer@hotmail.co.uk>
2018-03-30 12:14:35 +0100

    Add files via upload

---
+++ Enemies/EnemyStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour 
+{
+	public int m_CurHealth = 0; //Default valuefor now 
+	public int m_MaxHealth = 3; //3 By default (change in inpector for enemy variety)
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
---
+++ PlayerChar/SwordDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamage : MonoBehaviour 
+{
+	// ---- Sword Vars ----
+	int m_Damage = 1;
+
+	static public Rigidbody2D SwordBod;
+
+	void Start ()
+	{
+		SwordBod = GetComponent < Rigidbody2D> ();
+	}
+	/*int OnCollisonEnter(Collision other)
+	{
+		//Get Enemy Script/ apply damage to work with all enemies 
+		int health = GetComponent<EnemyStats> ().m_CurHealth;
+		health--;
+		return health;
+	}*/
+
+
+
+}
